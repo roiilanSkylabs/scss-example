448b3f4f6dcb7c1053653a5cef78e83151e92e6d
Roi Ilan <Roii@skylabs.co.il>
2021-12-13 13:45:46 +0200

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+<div class="about">
+
+    <h1 class="slide-down"> h1 - hellow world</h1>
+
+    <section class="s1">
+        <h2> s1 h2 - title </h2>
+        <p> lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor</p>
+    </section>
+
+    <section class="s2">
+        <h2> s2 h2 - title </h2>
+        <p> lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor</p>
+    </section>
+
+    <section class="s3">
+        <h2> s3 h2 - title </h2>
+        <p> lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor</p>
+    </section>
+
+    <section class="s4">
+        <h2> s4 h2 - title </h2>
+        <p> lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor</p>
+  
+
+        <div class= "box">
+            <p>
+                loremlorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor
+            </p>
+        </div>
+
+        <button class="submit" tabindex="6" type="submit">Submit</button>
+
+  </section>
+    <section class="container">
+  <div class="heart-emoji">
+    <div class="heart-shape"></div>
   </div>
+</section>
+
+
+</div>
 </template>
